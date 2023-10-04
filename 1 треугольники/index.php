361cbe0da52c54a5513ca9770ef33f813545592c
arinaroy7 <146703972+arinaroy7@users.noreply.github.com>
2023-10-04 14:34:44 +0400

    Triangles
    Написать три функции каждая из которых принимает на вход три точки и первая функция возвращает углы треугольника, образованного этими точками. Вторая - возвращает площадь этого треугольника и 3 возвращает длины стороны. Точки задаются в трех-мерном пространстве.

---
+++ 1 треугольники/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Функции</title>
+    <link rel="stylesheet" type="text/css" href="index.css"> </link> 
+</head>
+</html>
+<body>
+    <?php
+
+    function calculateTriangleInfo($point1, $point2, $point3) {
+
+        $a = sqrt(($point2['x'] - $point3['x']) ** 2 + ($point2['y'] - $point3['y']) ** 2);
+        $b = sqrt(($point1['x'] - $point3['x']) ** 2 + ($point1['y'] - $point3['y']) ** 2); //считаем длины сторон треуголь
+        $c = sqrt(($point1['x'] - $point2['x']) ** 2 + ($point1['y'] - $point2['y']) ** 2);
+
+        $s = calculateTriangleArea($a, $b, $c); //по формуле Герона - в тетраде
+
+        $sinA = ($a / $b) * sqrt(1 - ($a * $a + $b * $b - $c * $c) / (2 * $a * $b)); //через закон sin, считаем углы в радианах
+        $sinB = ($b / $c) * sqrt(1 - ($b * $b + $c * $c - $a * $a) / (2 * $b * $c));
+        $sinC = ($c / $a) * sqrt(1 - ($c * $c + $a * $a - $b * $b) / (2 * $a * $c));
+        
+        $angleA = rad2deg(asin($sinA)); 
+        $angleB = rad2deg(asin($sinB)); // из радианов в градусы переводим
+        $angleC = rad2deg(asin($sinC));
+
+        //возвр инф о треуголь
+        return [  
+            'a' => $a,
+            'b' => $b,
+            'c' => $c,
+            'angles' => [
+                'A' => $angleA,
+                'B' => $angleB,
+                'C' => $angleC,
+            ],
+            'area' => $s,
+        ];
+    }
+
+    function distance($point1, $point2) {
+
+        $dx = $point1['x'] - $point2['x']; 
+        $dy = $point1['y'] - $point2['y'];
+
+        return sqrt($dx * $dx + $dy * $dy); //через теорему пифагора - расстояние 
+    }
+
+    $point1 = ['x' => 0, 'y' => 0]; //определяем точки в двумерном пространстве пр: (0,0)
+    $point2 = ['x' => 4, 'y' => 0];
+    $point3 = ['x' => 0, 'y' => 3];
+
+    $angles = calculateTriangleInfo($point1, $point2, $point3); //функция вычисляет углы треуголь, образованного точками выше и возвр результат
+        
+    foreach ($angles as $angle) {
+        echo $angle . "<br>"; // получаем результат в виде массива углов ; тег "<br>" - перенос строки 
+    }
+
+    function calculateTriangleArea($a, $b, $c) {
+        // Вычисляем площадь треугольника по формуле Герона
+        $s = 0.5 * sqrt(($a + $b + $c) * ($b + $c - $a) * ($c + $a - $b) * ($a + $b - $c));
+        return $s;
+    }
+
+    $point1 = ['x' => 0, 'y' => 0];
+    $point2 = ['x' => 4, 'y' => 0];
+    $point3 = ['x' => 0, 'y' => 3];
+
+    $info = calculateTriangleInfo($point1, $point2, $point3);
+
+    echo "Длины сторон треугольника:";
+    echo "a: " . $info['a'] . "<br>";
+    echo "b: " . $info['b'] . "<br>";
+    echo "c: " . $info['c'] . "<br>";
+
+    echo "Углы треугольника (в градусах):";
+    echo "A: " . $info['angles']['A'] . "<br>";
+    echo "B: " . $info['angles']['B'] . "<br>";
+    echo "C: " . $info['angles']['C'] . "<br>";
+
+    echo "Площадь треугольника: " . $info['area'] . "<br>";
+    ?>
+</body>
